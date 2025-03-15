ed9f13b5d27a360da7c341eb15966c8f90b18c77
trinhtuandan03 <trinhtuandan1601@gmail.com>
2025-03-16 03:27:05 +0700

    updateupdate

--- frontend/app.js
+++ frontend/app.js
@@ -3,13 +3,13 @@ var app = express();
 
 
 // Cấu hình views và static files
-app.set("views", __dirname + "/frontend/src/views");
+app.set("views", __dirname + "/src/views");
 app.set("view engine", "ejs");
 // ✅ Cấu hình đúng đường dẫn đến static files
-app.use("/static", express.static(__dirname + "/frontend/public"));
+app.use("/static", express.static(__dirname + "/public"));
 
 // Import controller
-var controller = require(__dirname + "/frontend/src/controllers");
+var controller = require(__dirname + "/src/controllers");
 app.use(controller);
 
 var server = app.listen(3000, function() {
