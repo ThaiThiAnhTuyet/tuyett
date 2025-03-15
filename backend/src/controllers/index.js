e8d25555fce376a7d94ee453d6c8ca9ff18f14a5
trinhtuandan03 <trinhtuandan1601@gmail.com>
2025-03-16 04:00:48 +0700

    update

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 
 router.use("/", require(__dirname + "/loginregistercontroller"));
+router.use("/", require(__dirname + "/productcontroller"));
+router.use("/", require(__dirname + "/categorycontroller"));
 
 router.get("/", function(req, res) {
     res.render("index.ejs");
